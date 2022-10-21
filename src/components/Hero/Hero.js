1c4ed78e13a8da069b8f86887059aabba9fe1d4a
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-20 16:42:52 -1000

    initialized app

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+        I am a software engineer who is always learning and creating. 
+        </SectionText>
+        <Button onClick={props.handleClick}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
