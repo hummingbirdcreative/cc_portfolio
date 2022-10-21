938d3f0be361657b397512b003214f7e669701d3
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-20 16:47:02 -1000

    added links to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,10 +32,10 @@ const Header = () =>  (
       </li>        
     </Div2>
       <Div3>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://github.com/hummingbirdcreative">
           <AiFillGithub size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://www.linkedin.com/in/christinegrayb/">
           <AiFillLinkedin size="3rem" />
         </SocialIcons>
         <SocialIcons href="https://google.com">
