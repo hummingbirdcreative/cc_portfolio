307734341d7e74b0938a491b83fa9eb1e3ce8673
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-27 04:59:49 -1000

    git calendar style edits

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -5,10 +5,10 @@ const Timeline = () => {
   return (
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
-      <SectionText>
+      <SectionText style={{ paddingBottom: "0" }}>
       I am a full-stack web developer who enjoys creating precise code to solve problems creatively. With a background in design and customer service, I am passionate about visual aesthetics and good design that works uniquely for each client’s needs. I am a problem solver who has cultivated the ability to adapt quickly and find concise solutions.      </SectionText>
       <p style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px", color: "#ee5b25" }}>
+      <h1 className="project-heading" style={{ paddingTop: "0", paddingBottom: "20px", color: "#ee5b25" }}>
         Days I Code
       </h1>
       <GitHubCalendar
