df838fbc329f27fb7a55001b199f03f6d92cb291
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-28 03:50:58 -1000

    svg section spacing fixed

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   background-color: #Defe7b;
   padding: 1rem;
   padding-top: 5rem;
+  margin-top:-5px;
   
   @media ${(props) => props.theme.breakpoints.sm} {
     display: grid;
