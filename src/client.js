73ed20f5f828da8baffca9be885004f7f61d8633
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-11-01 16:19:52 -1000

    sanity removed

--- src/client.js
+++
@@ -1,8 +0,0 @@
-// client.js
-import sanityClient from '@sanity/client'
-
-export default sanityClient({
-  projectId: '9d38x21c', // you can find this in sanity.json
-  dataset: 'production', // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
-})
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,12 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import Contact from '../components/Contact/Contact';
 import SVG from '../components/SVG/SVG';
-import Blog from '../components/Blog/Blog';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+//import posts from './post/[slug]'
 
-const Home = () => {
+const Home = ({posts}) => {
+  console.log(posts)
   return (
     <Layout>
       <Section grid> 
@@ -20,10 +21,21 @@ const Home = () => {
       <Projects />
       <Technologies />
       <Timeline />
-      <Blog />
       <Contact />
     </Layout>
   );
 };
+
+
+export async function getStaticProps() {
+  const posts = await client.fetch(groq`
+    *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+  `)
+  return {
+    props: {
+      posts
+    }
+  }
+}
 
 export default Home;
