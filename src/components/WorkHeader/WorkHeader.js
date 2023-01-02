8621ace980724880bcce6231f7ca52b7f689a2bd
Andrew Martin <hello@andrewmart.in>
2023-01-01 14:43:02 -1000

    wip react 18

--- src/components/WorkHeader/WorkHeader.js
+++ src/components/WorkHeader/WorkHeader.js
@@ -3,16 +3,17 @@ import { GiDiamonds } from 'react-icons/gi';
 
 import { Container, Marquee } from './WorkHeaderStyles';
 
-const WorkHeader = () =>  (
-  <Container>
-    <Marquee>
+const WorkHeader = () => (
+  <div className="p-[0.5px] overflow-hidden">
+    <div className="flex">
       <marquee direction="left" scrollamount="8" behavior="scroll">
-        <GiDiamonds size="1rem" /> &nbsp; Available For Work &nbsp; <GiDiamonds size="1rem" />
+        <GiDiamonds size="1rem" /> &nbsp; Available For Work &nbsp;{' '}
+        <GiDiamonds size="1rem" />
         &nbsp; Available For Work &nbsp; <GiDiamonds size="1rem" />
         &nbsp; Available For Work &nbsp; <GiDiamonds size="1rem" />
       </marquee>
-    </Marquee>
-  </Container>
+    </div>
+  </div>
 );
 
 export default WorkHeader;
