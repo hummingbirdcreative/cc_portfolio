f91c94fcb1ad9a660c2f396f1ac1e70acbc920b2
Andrew Martin <hello@andrewmart.in>
2023-01-04 19:24:08 -1000

    yarn create next-app --typescript and replace old dir

--- src/layout/LayoutStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-//background-image: url("https://i.imgur.com/xw7hBol.png");
-//max-width: 1280px;
-width: 100%;
-margin: auto;
-`;
